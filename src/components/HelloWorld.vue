ea3c82021114e0f17604297144a6ace13a038ffc
zhangxiaokang <zhangxiaokang@bainuo.com>
2018-09-10 20:37:17 +0800

    '样式'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,14 @@
     <h1>{{ msg }}</h1>
     <h3>{{msg2}}</h3>
     <h2>Essential Links</h2>
+    <p>
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>
+    </p>
     <ul>
       <li>
         <a
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,48 +1,65 @@
 <template>
   <el-table
-    :data="tableData"
-    style="width: 100%">
+    :data='tableData'
+    style='width: 100%'>
     <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
+      prop='date'
+      label='日期'
+      width='180'>
     </el-table-column>
     <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
+      prop='name'
+      label='姓名'
+      width='180'>
     </el-table-column>
     <el-table-column
-      prop="address"
-      label="地址">
+      prop='address'
+      label='地址'>
     </el-table-column>
-    <el-table-column label="操作">
+    <el-table-column label='操作'>
+      <template slot-scope='scope'>
+        <el-button
+          @click.native.prevent='deleteRow(scope.$index, tableData)'
+          type='text'
+          size='small'>
+          移除
+        </el-button>
+      </template>
     </el-table-column>
   </el-table>
 </template>
-  
 <script>
-  export default {
-    data() {
-      return {
-        tableData: [{
+export default {
+  methods: {
+    deleteRow (index, rows) {
+      rows.splice(index, 1)
+    }
+  },
+  data () {
+    return {
+      tableData: [
+        {
           date: '2016-05-02',
           name: '王小虎0',
           address: '上海市普陀区金沙江路 1518 弄'
-        }, {
+        },
+        {
           date: '2016-05-04',
           name: '王小虎1',
           address: '上海市普陀区金沙江路 1517 弄'
-        }, {
+        },
+        {
           date: '2016-05-01',
           name: '王小虎2',
           address: '上海市普陀区金沙江路 1519 弄'
-        }, {
+        },
+        {
           date: '2016-05-03',
           name: '王小虎3',
           address: '上海市普陀区金沙江路 1516 弄'
-        }]
-      }
+        }
+      ]
     }
   }
+}
 </script>
